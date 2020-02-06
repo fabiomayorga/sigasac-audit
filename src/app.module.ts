3dad94b4e3954d6da43347b2ad48fed04e759684
Arith Alexis Villalba Bravo <arithalexis@gmail.com>
2020-02-06 17:14:50 -0500

    init configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { EasyconfigModule } from 'nestjs-easyconfig';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuditorModule } from './auditor/auditor.module';
 
 @Module({
-    imports: [],
+    imports: [AuditorModule, EasyconfigModule.register({})],
     controllers: [AppController],
     providers: [AppService]
 })
